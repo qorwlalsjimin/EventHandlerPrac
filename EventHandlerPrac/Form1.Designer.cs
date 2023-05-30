6d11b51f21a90a40741622573c6506d871cc54ba
Jimin Baek <qorwlalsjimin@naver.com>
2023-05-30 11:15:25 +0900

    버튼 클릭 이벤트로 타이머 구현

--- EventHandlerPrac/Form1.Designer.cs
+++ EventHandlerPrac/Form1.Designer.cs
@@ -28,20 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(36, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(413, 169);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(36, 216);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(413, 193);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(466, 41);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(294, 368);
+            this.panel1.TabIndex = 2;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(37, 188);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(230, 147);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(37, 135);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(230, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "타이머";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- EventHandlerPrac/Form1.cs
+++ EventHandlerPrac/Form1.cs
@@ -16,5 +16,40 @@ namespace EventHandlerPrac
         {
             InitializeComponent();
         }
+
+        //버튼 클릭
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            MessageBox.Show(btn.Text+" 버튼이 클릭되었습니다.");
+
+/*            switch (btn)
+            {
+                case button1:
+                    break;
+            }*/
+        }
+
+        // frame창 닫힐때
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            MessageBox.Show(e.CloseReason.ToString()); //UserClosing
+        }
+
+
+        private int elapsedTime = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            elapsedTime++;
+            textBox1.Text = elapsedTime + "초 경과";
+            label1.Text = elapsedTime + "초 경과";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //타이머 켜추기
+            timer1.Enabled = !timer1.Enabled; //켜져있을땐 끄고, 꺼져있을땐 켜주기
+        }
     }
 }
